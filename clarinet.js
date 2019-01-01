fb3ed036606c2363db7ffd2a031c53da37e41700
DLehenbauer <DLehenbauer@users.noreply.github.com>
2018-12-31 18:08:14 -0800

    Remove unused Char constants

--- clarinet.js
+++ clarinet.js
@@ -85,44 +85,11 @@ else env = window;
     period              : 0x2E,     // .
 
     _0                  : 0x30,     // 0
-    _1                  : 0x31,     // 1
-    _2                  : 0x32,     // 2
-    _3                  : 0x33,     // 3
-    _4                  : 0x34,     // 4
-    _5                  : 0x35,     // 5
-    _6                  : 0x36,     // 6
-    _7                  : 0x37,     // 7
-    _8                  : 0x38,     // 8
     _9                  : 0x39,     // 9
 
     colon               : 0x3A,     // :
 
-    A                   : 0x41,     // A
-    B                   : 0x42,     // B
-    C                   : 0x43,     // C
-    D                   : 0x44,     // D
     E                   : 0x45,     // E
-    F                   : 0x46,     // F
-    G                   : 0x47,     // G
-    H                   : 0x48,     // H
-    I                   : 0x49,     // I
-    J                   : 0x4A,     // J
-    K                   : 0x4B,     // K
-    L                   : 0x4C,     // L
-    M                   : 0x4D,     // M
-    N                   : 0x4E,     // N
-    O                   : 0x4F,     // O
-    P                   : 0x50,     // P
-    Q                   : 0x51,     // Q
-    R                   : 0x52,     // R
-    S                   : 0x53,     // S
-    T                   : 0x54,     // T
-    U                   : 0x55,     // U
-    V                   : 0x56,     // V
-    W                   : 0x57,     // W
-    X                   : 0x58,     // X
-    Y                   : 0x59,     // Y
-    Z                   : 0x5a,     // Z
 
     openBracket         : 0x5B,     // [
     backslash           : 0x5C,     // \
@@ -130,30 +97,14 @@ else env = window;
 
     a                   : 0x61,     // a
     b                   : 0x62,     // b
-    c                   : 0x63,     // c
-    d                   : 0x64,     // d
-    e                   : 0x65,     // e
+    e                   : 0x65,     // e 
     f                   : 0x66,     // f
-    g                   : 0x67,     // g
-    h                   : 0x68,     // h
-    i                   : 0x69,     // i
-    j                   : 0x6A,     // j
-    k                   : 0x6B,     // k
     l                   : 0x6C,     // l
-    m                   : 0x6D,     // m
     n                   : 0x6E,     // n
-    o                   : 0x6F,     // o
-    p                   : 0x70,     // p
-    q                   : 0x71,     // q
     r                   : 0x72,     // r
     s                   : 0x73,     // s
     t                   : 0x74,     // t
     u                   : 0x75,     // u
-    v                   : 0x76,     // v
-    w                   : 0x77,     // w
-    x                   : 0x78,     // x
-    y                   : 0x79,     // y
-    z                   : 0x7A,     // z
 
     openBrace           : 0x7B,     // {
     closeBrace          : 0x7D,     // }
